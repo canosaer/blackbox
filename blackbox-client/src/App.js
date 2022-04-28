84dc649b780628e97097e4b532a2811959af5d46
canosaer <67963828+canosaer@users.noreply.github.com>
2022-04-27 21:07:40 -0400

    1.0

--- blackbox-client/src/App.js
+++ blackbox-client/src/App.js
@@ -1,19 +1,35 @@
 import './styles/main.scss'
 
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Terminal, useEventQueue } from 'crt-terminal';
-// import Header from './components/Header'
-// import Hero from './components/Hero'
-// import Projects from './components/Projects'
-// import Footer from './components/Footer'
+import Moment from 'react-moment';
+import 'moment-timezone';
+
 import axios from 'axios';
 
 function App() {
   const [outbound, setOutbound] = useState('')
   const [messages, setMessages] = useState([])
-  const eventQueue = useEventQueue();
+  const eventQueue = useEventQueue()
 
+  const messagesEnd = useRef(null)
 
+  const scrollToBottom = () => {
+    messagesEnd.current?.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [messages]);
+
+  const retrieveMessages = async () => {
+    try {
+      const response = await axios.get('http://localhost:1337/api/message')
+      setMessages(response)
+    } catch (error) {
+        console.log(error);
+    }
+  }
 
   const sendMessage = async (pressedKey) => {
     if(outbound && pressedKey === 'Enter'){
@@ -21,21 +37,42 @@ function App() {
       .post('http://localhost:1337/api/message', {
           content: outbound,
       })
-      .then(function (response) {
-          setMessages(response)
-          console.log(messages)
-      })
+      .then(retrieveMessages)
       .catch(function (error) {
           console.log(error);
-      });
+      })
       setOutbound('')
     }
   }
+
+  useEffect(() => {
+    retrieveMessages()
+  }, []);
 
   return (
     <>
         <main className="terminal">
           <Terminal queue={eventQueue} />
+          {messages.data ? 
+            <section className ="message-display">
+            {messages.data.map((message, i) => {
+              const key = message.id
+
+              return(
+                <div key={key} className="message">
+                  <aside className="message__timestamp">
+                    <Moment format="MMM DD h:mm">
+                      {message.timestamp}
+                    </Moment>
+                  </aside>
+                  <p className="message__content">{message.content}</p>
+                </div>
+              )
+            })}
+            <div ref={messagesEnd} />
+          </section>
+          :
+          null}
           <div className="input">
             <p className="input__prompt"> &gt; </p>
             <input className="input__text" type="text" value={outbound} onChange={(e) => setOutbound(e.target.value)} onKeyDown={(e) => sendMessage(e.key)} autoFocus={true} onBlur={({ target }) => target.focus()} />
